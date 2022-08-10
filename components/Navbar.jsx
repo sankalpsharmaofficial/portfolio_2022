49c13918a200c79d439b48fb151fae505a4b4865
Sankalp Sharma <sankalp.sharma156@gmail.com>
2022-08-10 17:00:42 +0530

    navbar menu buttons set to bold

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -62,23 +62,27 @@ const Navbar = () => {
 				<div>
 					<ul style={{ color: `${linkColor}` }} className="hidden md:flex">
 						<Link href="/">
-							<li className="ml-10 text-sm uppercase  hover:border-b">Home</li>
+							<li className="ml-10 text-sm uppercase font-bold hover:border-b">
+								Home
+							</li>
 						</Link>
 						<Link href="/#about">
-							<li className="ml-10 text-sm uppercase  hover:border-b">About</li>
+							<li className="ml-10 text-sm uppercase font-bold hover:border-b">
+								About
+							</li>
 						</Link>
 						<Link href="/#skills">
-							<li className="ml-10 text-sm uppercase  hover:border-b">
+							<li className="ml-10 text-sm uppercase font-bold hover:border-b">
 								Skills
 							</li>
 						</Link>
 						<Link href="/#projects">
-							<li className="ml-10 text-sm uppercase  hover:border-b">
+							<li className="ml-10 text-sm uppercase font-bold hover:border-b">
 								Projects
 							</li>
 						</Link>
 						<Link href="/#contact">
-							<li className="ml-10 text-sm uppercase  hover:border-b">
+							<li className="ml-10 text-sm uppercase font-bold hover:border-b">
 								Contact
 							</li>
 						</Link>
